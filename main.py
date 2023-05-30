cfa8bae22f16310f7218f7bb927af274cf83d8c3
Matheus Kepy <matheuskdev@gmail.com>
2023-05-29 22:17:42 -0300

    :sparkles: Inclusão de nova rota e base tamplate

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, \
+render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
+def home():
+    return render_template("home.html")
 
-def homepage():
-    return "Hello Word"
+@app.route("/profile/<user>")
+def profile(user):
+    return render_template("profile.html", user=user)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
