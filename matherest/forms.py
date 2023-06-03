cfc8c0a3a110111d64104016199c9caf909b52c1
Matheus Kepy <matheuskdev@gmail.com>
2023-06-03 19:49:37 -0300

    :sparkles: feat: Implementação das rotas Login, Logout e Profile

--- matherest/forms.py
+++ matherest/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from matherest.models import User
 
 class FormLogin(FlaskForm):
     email =             StringField("Email", validators=[DataRequired(), Email()])
-    password =             PasswordField("Senha", validators=[DataRequired()])
-    button_confirm = SubmitField("Fazer Login")
+    password =          PasswordField("Senha", validators=[DataRequired()])
+    button_confirm =    SubmitField("Fazer Login")
 
 class FormCreateAccount(FlaskForm):
     username=           StringField("Nome de Usuário", validators=[DataRequired()])
@@ -20,3 +20,6 @@ class FormCreateAccount(FlaskForm):
         if user:
             return ValidationError("Email já cadastrado, faça login para continuar")
         
+class FormPhoto(FlaskForm):
+    photo = FileField("Foto", validators=[DataRequired()])
+    button_confirm =    SubmitField("Criar Usuário")
--- matherest/routes.py
+++ matherest/routes.py
@@ -1,21 +1,69 @@
 from flask import render_template, url_for
-from matherest import app
-from flask_login import login_required
-from matherest.forms import FormLogin, FormCreateAccount
+from flask_login import login_required, login_user, logout_user, current_user
+from matherest import *
+from matherest.forms import FormLogin, FormCreateAccount, FormPhoto
+from matherest.models import User, Photo, Like
 
 
+
+#Home and Login route
 @app.route("/", methods=["GET", "POST"])
 def home():
-    formlogin = FormLogin()
-    return render_template("home.html", form=formlogin)
+    form_login = FormLogin()
+    if form_login.validate_on_submit():
+        user = User.query.filter_by(
+              email=form_login.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, 
+                                       form_login.password.data):
+            login_user(user)
+            login_user(user, remember=True)
+            return redirect(url_for("profile", 
+                                    id_user=user.id))
+    
+    return render_template("home.html", form=form_login)
 
+
+
+# Create Account route
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
-    formcreateaccount = FormCreateAccount()
-    return render_template("create_account.html", form=formcreateaccount)
+    form_createaccount = FormCreateAccount()
+    
+    if form_createaccount.validate_on_submit():
+        password = bcrypt.generate_password_hash(
+                    form_createaccount.password.data)
+        
+        user = User(username=form_createaccount.username.data,
+                    email=form_createaccount.email.data, 
+                    password=password)
+        
+        database.session.add(user)
+        database.session.commit()
+        login_user(user, remember=True)
+        return redirect(url_for("profile", id_user=user.id))
+    return render_template("create_account.html", 
+                           form=form_createaccount)
 
 
-@app.route("/profile/<user>")
+
+# Profile user route
+@app.route("/profile/<id_user>", methods=["GET", "POST"])
 @login_required
-def profile(user):
-    return render_template("profile.html", user=user)
+def profile(id_user):
+    if id_user == int(current_user.id):
+        # render profile user loged
+        form_photo = FormPhoto()
+        return render_template("profile.html", 
+                               user=current_user, form=form_photo)
+    else:
+        user = User.query.get(int(id_user))
+    return render_template("profile.html", user=id_user, form=None)
+
+
+
+#Logout user rout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
